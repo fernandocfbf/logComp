69b314af70879c244cb755dbffff59d7eabfb159
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-03-16 17:36:54 -0300

    feat: adding non-binary tokens

--- src/classes/Parser.py
+++ src/classes/Parser.py
@@ -1,4 +1,3 @@
-from ast import parse
 from src.classes.Token import Token
 from src.classes.Tokenizer import Tokenizer
 from src.constants.tokens import ALL_TOKENS, EXPRESSION_TOKENS, TERM_TOKENS
@@ -6,6 +5,33 @@ from src.constants.tokens import ALL_TOKENS, EXPRESSION_TOKENS, TERM_TOKENS
 class Parser():
     tokens = None
 
+    def parseFactor(tokenizer):
+        '''
+        input: Tokenizer object
+        output: number (int)
+        description: computes non-binary tokens (-, +)
+        '''
+        result = 0
+        if tokenizer.actual.type == "number":
+            result = int(tokenizer.actual.value)
+            tokenizer.selectNext()
+        elif tokenizer.actual.type == "+":
+            tokenizer.selectNext()
+            result += Parser.parseFactor(tokenizer)
+        elif tokenizer.actual.type == "-":
+            tokenizer.selectNext()
+            result -= Parser.parseFactor(tokenizer)
+        elif tokenizer.actual.type == "(":
+            tokenizer.selectNext()
+            result = Parser.parseExpression(tokenizer)
+            if tokenizer.actual.type == ")":
+                tokenizer.selectNext()
+            else:
+                raise Exception("Invalid syntax")
+        else:
+            raise Exception("Invalid expression")
+        return result
+        
     def parseTerm(tokenizer):
         '''
         input: Tokenizer object
@@ -13,27 +39,15 @@ class Parser():
         description: read all the tokens for the expression and calculates the result.
             Performs times and division only
         '''
-        result = 0
-        if tokenizer.actual.type == "number":
-            result = int(tokenizer.actual.value)
-            next_token = tokenizer.selectNext() # read operator
-            while next_token.type in TERM_TOKENS:
-                if next_token.type == "*":
-                    next_token = tokenizer.selectNext()
-                    if next_token.type == "number":
-                        result *= int(next_token.value)
-                    else:
-                        raise Exception("Invalid syntax")
-                elif next_token.type == "/":
-                    next_token = tokenizer.selectNext()
-                    if next_token.type == "number":
-                        result /= int(next_token.value)
-                    else:
-                        raise Exception("Invalid syntax")
-                next_token = tokenizer.selectNext()
-            return result
-        else:
-            raise Exception("Invalid expression")
+        result = Parser.parseFactor(tokenizer)
+        while tokenizer.actual.type in TERM_TOKENS:
+            if tokenizer.actual.type == "*":
+                tokenizer.selectNext()
+                result *= Parser.parseFactor(tokenizer)
+            elif tokenizer.actual.type == "/":
+                tokenizer.selectNext()
+                result /= Parser.parseFactor(tokenizer)
+        return result
 
     def parseExpression(tokenizer):
         '''
@@ -45,10 +59,10 @@ class Parser():
         result = Parser.parseTerm(tokenizer)
         while tokenizer.actual.type in EXPRESSION_TOKENS:
             if tokenizer.actual.type == "+":
-                current_token = tokenizer.selectNext()
+                tokenizer.selectNext()
                 result += Parser.parseTerm(tokenizer)
             elif tokenizer.actual.type == "-":
-                current_token = tokenizer.selectNext()
+                tokenizer.selectNext()
                 result -= Parser.parseTerm(tokenizer)
         return result
 
@@ -84,9 +98,6 @@ class Parser():
         tokens = Tokenizer(parse_expression, 0, Token(None, parse_expression[0]))
         tokens.selectNext()
         final_result = Parser.parseExpression(tokens)
+        if(tokens.actual.type != "EOF"):
+            raise Exception("Invalid syntax")
         return final_result
-
-
-
-
-
