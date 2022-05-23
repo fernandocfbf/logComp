47d57bd19821d9a6f6e607b370bc4df947e0b454
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-05-23 12:05:11 -0300

    feat: changing BinOp class

--- src/classes/BinOp.py
+++ src/classes/BinOp.py
@@ -1,46 +1,64 @@
 from src.classes.Node import Node
+from src.classes.CodeGenerator import CodeGenerator
 class BinOp(Node):
 
     #@Override
     def Evaluate(self):
         node1 = self.children[0].Evaluate()
+        CodeGenerator.write("push EBX")
         node2 = self.children[1].Evaluate()
+        CodeGenerator.write("pop EAX")
 
         if node1[0] == node2[0] == "int":
             if self.variant == "+":
-                return ("int", int(node1[1]+node2[1]))
+                CodeGenerator.write("add EAX, EBX")
+                #return ("int", int(node1[1]+node2[1]))
             elif self.variant == "-":
-                return ("int", int(node1[1]-node2[1]))
+                CodeGenerator.write("sub EAX, EBX")
+                #return ("int", int(node1[1]-node2[1]))
             elif self.variant == "*":
-                return ("int", int(node1[1]*node2[1]))
+                CodeGenerator.write("imul EAX, EBX")
+                #return ("int", int(node1[1]*node2[1]))
             elif self.variant == "/":
-                return ("int", int(node1[1]/node2[1]))
+                CodeGenerator.write("idiv EAX, EBX")
+                #return ("int", int(node1[1]/node2[1]))
             elif self.variant == "==":
-                return ("int", int(int(node1[1]) == int(node2[1])))
+                CodeGenerator.write("je EAX, EBX")
+                #return ("int", int(int(node1[1]) == int(node2[1])))
             elif self.variant == "<":
-                return ("int", int(int(node1[1]) < int(node2[1])))
+                CodeGenerator.write("jl EAX, EBX")
+                #return ("int", int(int(node1[1]) < int(node2[1])))
             elif self.variant == ">":
-                return ("int", int(int(node1[1]) > int(node2[1])))
+                CodeGenerator.write("jg EAX, EBX")
+                #return ("int", int(int(node1[1]) > int(node2[1])))
             elif self.variant == "||":
-                return ("int", node1[1] or node2[1])
+                CodeGenerator.write("OR EAX, EBX")
+                #return ("int", node1[1] or node2[1])
             elif self.variant == "&&":
-                return ("int", node1[1] and node2[1])
+                CodeGenerator.write("AND EAX, EBX")
+                #return ("int", node1[1] and node2[1])
             elif self.variant == ".":
-                return ("str", str(node1[1]) + str(node2[1]))
+                CodeGenerator.write("any-binOp EAX, EBX")
+                #return ("str", str(node1[1]) + str(node2[1]))
             else:
                 raise Exception("Invalid token")
         elif node1[0] == node2[0] == "str":
             if self.variant == "==":
-                return ("int", int(node1[1] == node2[1]))
+                CodeGenerator.write("je EAX, EBX")
+                #return ("int", int(node1[1] == node2[1]))
             elif self.variant == "<":
-                return ("int", int(node1[1] < node2[1]))
+                CodeGenerator.write("jl EAX, EBX")
+                #return ("int", int(node1[1] < node2[1]))
             elif self.variant == ">":
-                return ("int", int(node1[1] > node2[1]))
+                CodeGenerator.write("jg EAX, EBX")
+                #return ("int", int(node1[1] > node2[1]))
             elif self.variant == ".":
-                return ("str", str(node1[1]) + str(node2[1]))
+                CodeGenerator.write("any-binOp EAX, EBX")
+                #return ("str", str(node1[1]) + str(node2[1]))
             return ("str", str(node1[1]) + str(node2[1]))
         elif node1[0] != node2[1]:
             if self.variant == ".":
-                return ("str", str(node1[1]) + str(node2[1]))
+                CodeGenerator.write("any-binOp EAX, EBX")
+                #return ("str", str(node1[1]) + str(node2[1]))
 
         raise Exception("Invalid expression")
