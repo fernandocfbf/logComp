d6232824e9f74cc356e696df23dca174f88de057
fernando fincatti <fernando.fincatti@p4f.com>
2022-05-24 21:05:56 -0300

    feat: adding if statement

--- src/classes/If.py
+++ src/classes/If.py
@@ -1,10 +1,19 @@
 from src.classes.Node import Node
+from src.classes.CodeGenerator import CodeGenerator
 
 class If(Node):
     
     #Override
     def Evaluate(self):
-        if self.children[0].Evaluate():
-            return self.children[1].Evaluate()
-        elif len(self.children) > 2:
+        CodeGenerator.write("if_{0}:".format(self.i))
+        self.children[0].Evaluate()
+        CodeGenerator.write("cmp EBX, False")
+        CodeGenerator.write("je else_{0}".format(self.i))
+        self.children[1].Evaluate()
+        CodeGenerator.write("jmp end_if_{0}".format(self.i))
+        CodeGenerator.write("else_{0}:".format(self.i))
+
+        if len(self.children) > 2:
             self.children[2].Evaluate()
+        CodeGenerator.write("end_if_{0}:".format(self.i))
+        
