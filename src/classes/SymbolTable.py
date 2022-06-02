c1d5e7ddd3964143ad2861485615a3b6ebd6d0c0
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-06-02 11:27:24 -0300

    feat: adding symbol table

--- src/classes/SymbolTable.py
+++ src/classes/SymbolTable.py
@@ -1,22 +1,23 @@
 from src.constants.types import TYPES
 
 class SymbolTable():
-    symbols = dict()
-
-    def create(symbol):
-        if symbol[1] not in SymbolTable.symbols:
-            SymbolTable.symbols[symbol[1]] = (symbol[0], None)
+    def __init__(self):
+        self.symbols = dict()
+        
+    def create(self, symbol):
+        if symbol[1] not in self.symbols:
+            self.symbols[symbol[1]] = (symbol[0], None)
         else:
             raise Exception("Cannot subscribe variable")
 
-    def setSymbol(symbol_name, symbol_value):
+    def setSymbol(self, symbol_name, symbol_value):
         if symbol_value[0] in TYPES:
-            if (SymbolTable.symbols[symbol_name][0] == symbol_value[0]):
-                SymbolTable.symbols[symbol_name] = (symbol_value[0], symbol_value[1])
+            if (self.symbols[symbol_name][0] == symbol_value[0]):
+                self.symbols[symbol_name] = (symbol_value[0], symbol_value[1])
                 return
             raise Exception("Invalid assingment")
         raise Exception("Invalid type")
 
-    def getSymbol(symbol_name):
-        return SymbolTable.symbols[symbol_name]
+    def getSymbol(self, symbol_name):
+        return self.symbols[symbol_name]
 
